9d7a1fe4bb980a9db3ed31670a806b4c4503133e
Сергей Соколов <sergsok1984@gmail.com>
2022-09-05 15:32:19 +0300

    add Exercise 38

---
+++ Exercise_38/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Минимальный элемент массива: {min}");
+Console.WriteLine($"Максимальный элемент массива: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {max-min}");
+
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
