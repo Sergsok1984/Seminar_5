c994429bbb523fbaf4016fdb223f88a0261b11d5
Сергей Соколов <sergsok1984@gmail.com>
2022-09-05 15:20:17 +0300

    add Exercise 36

---
+++ Exercise_36/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sumNumbersEvenIndex = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sumNumbersEvenIndex += numbers[i];
+}
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях: {sumNumbersEvenIndex} ");
+
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
